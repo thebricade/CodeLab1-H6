0322bff7b9616a33efcf2bf80d38d277721f76c8
Bri <bs3864@nyu.edu>
2019-05-05 22:50:26 -0400

    Look @ Player script, added barista character

--- CodeLab-HW6/Assets/Scripts/LookAtPlayer.cs
+++ CodeLab-HW6/Assets/Scripts/LookAtPlayer.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class LookAtPlayer : MonoBehaviour
 {
+    public Transform LookPlayer;
+    public float rotateSpeed; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotateSpeed = 2; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (LookPlayer == null)
+        {
+            return;
+        } // if there's nothing to look at don't go futher
+
+        Vector3 forward = transform.position - LookPlayer.position;
+        Quaternion targetRotation = Quaternion.LookRotation(forward);
         
+        //slerp the information smooth
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+       // transform.rotation = new Vector3(forward);
     }
 }
